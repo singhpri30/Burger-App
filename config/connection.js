f3b69b70801ff44045a228faecc1888e58fdb4e4
Pri_030 <priyankasingh30@gmail.com>
2020-06-24 23:25:48 -0400

    updated connection file

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,33 @@
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+require("dotenv").config();
 
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+    connection = mysql.createConnection({
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // database: process.env.DB_NAME,
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'admin@123',
+        database: 'burgers_db',
+
+    });
+};
+
 
 connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: ");
-        return;
-    }
+    if (err) throw err;
+
+
     console.log("Database Connected");
 });
 
---
+++ models/burger.js
@@ -0,0 +1,4 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+
