2935782c1e2f75f952d7bff5240df075624c28e2
Pri_030 <priyankasingh30@gmail.com>
2020-06-28 21:31:32 -0400

    updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
-const mysql = require("mysql");
 require("dotenv").config();
+const mysql = require("mysql");
+
 
 let connection;
 if (process.env.JAWSDB_URL) {
@@ -8,16 +9,11 @@ if (process.env.JAWSDB_URL) {
 }
 else {
     connection = mysql.createConnection({
-        // host: process.env.DB_HOST,
-        // port: process.env.DB_PORT,
-        // user: process.env.DB_USER,
-        // password: process.env.DB_PASSWORD,
-        // database: process.env.DB_NAME,
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'admin@123',
-        database: 'burgers_db',
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
 
     });
 };
